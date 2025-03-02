b212717fec8273539625771f9ef7d671da739743
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-02 12:24:20 -0300

    feat: add is visible and picture

--- contact/models.py
+++ contact/models.py
@@ -14,6 +14,9 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=20)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(default=timezone.now)
+    is_visible = models.BooleanField(default=True)
+    picture = models.ImageField(
+        blank=True, null=True, upload_to='pictures/contacts/%Y/%m/')
 
     def __str__(self):
         return f'{self.name} / {self.email} / {self.phone_number}'

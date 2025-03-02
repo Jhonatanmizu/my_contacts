7e1320ea6471474e56b791e851dbe8feda1b9600
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-02 01:32:23 -0300

    feat: create contact model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+"""
+ Contact models
+"""
+from django.db import models  # type: ignore
+from django.utils import timezone  # type: ignore
 
-# Create your models here.
+
+class Contact(models.Model):
+    """ 
+    Contact model
+    """
+    name = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
+    phone_number = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name} / {self.email} / {self.phone_number}'

012cd09928d9769f59a7114f5781cb96cba9f43d
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-02 01:33:11 -0300

    feat: add contacts in admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from contact.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    """" Contact admin """
+    list_display = ("id", 'name', 'email', 'phone_number', 'created_at')
+    ordering = ('created_at',)
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'phone_number', 'created_at')
+    list_per_page = 20
+    list_max_show_all = 100

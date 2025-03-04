92743c6d3fff623a76f17c967e1ccc5ebad00ee0
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-04 19:02:58 -0300

    feat: implement search content

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
 """ Contacts Views """
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from contact.models import Contact
 
@@ -12,6 +13,25 @@ def index(request: HttpRequest) -> HttpResponse:
         Contact index view
     """
     contacts = Contact.objects.filter(is_visible=True).order_by("-id")[:10]
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/pages/index.html', context)
+
+
+def search_contacts(request: HttpRequest) -> HttpResponse:
+    """_summary_
+        Contact index view
+    """
+    search_term = request.GET.get('q', '').strip()
+
+    if not search_term:
+        return redirect('contact:index')
+
+    contacts = Contact.objects.filter(
+        Q(name__icontains=search_term) | Q(email__icontains=search_term),
+        is_visible=True
+    ).order_by("-id")
     context = {
         'contacts': contacts
     }

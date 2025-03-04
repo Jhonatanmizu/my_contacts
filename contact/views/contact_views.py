5773f85cd4813d250b2794c6fbccef87d0e18c45
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-04 18:25:27 -0300

    feat: create contact view and page

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,17 +1,30 @@
 """ Contacts Views """
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from contact.models import Contact
 
 # Create your views here.
 
 
 def index(request: HttpRequest) -> HttpResponse:
     """_summary_
-    Contact index view
-    Args:
-        request (HttpRequest): _description_
-
-    Returns:
-        HttpResponse: _description_
+        Contact index view
     """
-    return render(request, 'contact/pages/index.html')
+    contacts = Contact.objects.filter(is_visible=True).order_by("-id")[:10]
+    context = {
+        'contacts': contacts
+    }
+    return render(request, 'contact/pages/index.html', context)
+
+
+def contact(request: HttpRequest, contact_id: int) -> HttpResponse:
+    """_summary_
+        Contact view
+    """
+    target_contact = get_object_or_404(Contact, pk=contact_id, is_visible=True)
+
+    context = {
+        'contact': target_contact
+    }
+    return render(request, 'contact/pages/contact.html', context)

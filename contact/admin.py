c4f409810cae99eaf90ba0f9ef86188fe778ebad
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-04 18:24:29 -0300

    feat: add is visible in contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -7,12 +7,14 @@ from contact.models import Category, Contact
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
     """" Contact admin """
-    list_display = ("id", 'name', 'email', 'phone_number', 'created_at')
+    list_display = ("id", 'name', 'email', 'phone_number',
+                    'created_at', 'is_visible')
     ordering = ('created_at',)
     list_filter = ('created_at',)
     search_fields = ('name', 'email', 'phone_number', 'created_at')
     list_per_page = 20
     list_max_show_all = 100
+    list_editable = ('is_visible',)
 
 
 @admin.register(Category)

5dee6b469e3ef9db739f934e62fc18c9a7ac0705
jhonatanmizu <natanjesuss20@gmail.com>
2025-03-17 23:16:22 -0300

    feat: create update, signin and signout urls path

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,7 @@ app_name = 'authentication'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
+    path('update/', views.user_update, name='update'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
 ]
